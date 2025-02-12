476406b099696c671d5e504f9c650890b17d5c8d
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-02-12 14:18:17 +0300

    Add products filter component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import ProductCard from "@/components/product-card";
+import { ProductsFilter } from "@/components/products-filter";
 import { Product } from "@/components/shared/types";
-
 import { getProducts } from "@/server/products";
 
 export default async function Home() {
@@ -9,6 +9,8 @@ export default async function Home() {
   return (
     <main className="flex flex-col gap-10 justify-center max-w-6xl mx-auto p-10">
       <h1>Awesome Products</h1>
+
+      <ProductsFilter />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {products.map((product: Product) => (
---
+++ components/products-filter.tsx
@@ -0,0 +1,32 @@
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export function ProductsFilter() {
+  return (
+    <div className="flex gap-2 justify-between">
+      <div>
+        <Input placeholder="Search" className="w-full" />
+      </div>
+      <div>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Per Page" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="10">10</SelectItem>
+            <SelectItem value="20">20</SelectItem>
+            <SelectItem value="30">30</SelectItem>
+            <SelectItem value="40">40</SelectItem>
+            <SelectItem value="50">50</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
+}
