7093a53eb6223181d8d37488e74f159cb5d44999
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-02-12 03:56:06 +0300

    Fetch and render products dynamically on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Product } from "@/components/shared/types";
 import {
   Card,
   CardContent,
@@ -6,25 +7,30 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { getProducts } from "@/server/products";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getProducts();
+
   return (
     <main className="flex flex-col gap-10 justify-center max-w-6xl mx-auto p-10">
       <h1>Awesome Products</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        {products.map((product: Product) => (
+          <Card key={product.id}>
+            <CardHeader>
+              <CardTitle>{product.title}</CardTitle>
+              <CardDescription>{product.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </main>
   );
---
+++ server/products.ts
@@ -0,0 +1,7 @@
+"use server";
+
+export async function getProducts() {
+  const response = await fetch("https://api.escuelajs.co/api/v1/products");
+  const data = await response.json();
+  return data;
+}
